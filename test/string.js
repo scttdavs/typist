a438a7bccc743927d954ac74386a86d754b572aa
Scott Davis <sc@ttdavis.com>
2016-04-07 12:03:35 -0400

    add array and string

--- test/string.js
+++ test/string.js
@@ -4,44 +4,45 @@ var chai = require("chai");
 var expect = chai.expect;
 var typist = require("../typist");
 
-describe("Basics", function() {
+describe("Strings", function() {
   it("should check a type and return the value", function() {
-    var arr = [1, 2, 3];
-    expect(typist.array(arr)).to.equal(arr);
+    var str = "str";
+    expect(typist.string(str)).to.equal(str);
   });
 
   it("should check a type and return an error", function() {
-    var str = "type";
+    var arr = [1, 2, 3];
     var curry = function() {
-      typist.array(str);
+      typist.string(arr);
     }
 
-    expect(curry).to.throw("TypistError: Expected variable to be of type Array");
+    expect(curry).to.throw("TypistError: Expected variable to be of type String");
   });
 
   it("should check a type and return a boolean", function() {
     var arr = [1, 2, 3];
     var str = "type";
-    expect(typist.is.array(arr)).to.be.true;
-    expect(typist.is.array(str)).to.be.false;
+    expect(typist.is.string(arr)).to.be.false;
+    expect(typist.is.string(str)).to.be.true;
   });
 
   it("should 'annotate' a function return type", function() {
-    var test = typist(Array, function(input) {
+    var test = typist(String, function(input) {
       return input;
     });
     
-    expect(test([])).to.be.eql([]);
+    expect(test("str")).to.be.eql("str");
   });
 
   it("should 'annotate' a function return error", function() {
-    var test = typist(Array, function(input) {
+    var test = typist(String, function(input) {
       return input;
     });
     var curry = function() {
-      test("str");
+      test([]);
     }
     
-    expect(curry).to.throw("TypistError: Expected a return value to be of type Array");
+    expect(curry).to.throw("TypistError: Expected a return value to be of type String");
   });
 });
+
--- typist.js
+++ typist.js
@@ -5,7 +5,59 @@
 }('typist', this, function () {
   'use strict';
 
-  var typist;
+  // Custom typist error for debugging
+  var TypistError = function(message) {
+  	this.message = message;
+  };
+  TypistError.prototype = Object.create(TypeError.prototype);
+
+  var typist = function(type, func) {
+  	return function() {
+  		var result = func.apply(this, arguments);
+  		if (result instanceof type || typeof result === type.name.toLowerCase()) {
+  			return result;
+  		} else {
+  			throw new TypistError("TypistError: Expected a return value to be of type " + type.name);
+  		}
+  	}
+  };
+
+
+  
+
+  // "is" checks
+
+  typist.is = {};
+
+  typist.is.array = function(test) {
+  	return Array.isArray(test);
+  }
+
+  typist.is.string = function(test) {
+  	return test instanceof String || typeof test === String.name.toLowerCase();
+  }
+
+  // Main Stuff
+
+  var check = function(test, value, type) {
+  	if (test) {
+  		return value;
+  	} else {
+  		throw new TypistError("TypistError: Expected variable to be of type " + type);
+  	}
+  }
+
+  typist.array = function(test) {
+  	return check(this.is.array(test), test, "Array");
+  };
+
+  typist.string = function(test) {
+  	return check(this.is.string(test), test, "String");
+  };
+
+
+
+  
 
   return typist;
 }); // jshint ignore:line
